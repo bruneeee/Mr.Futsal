6561e1ab35a4bdb606d2320b9bad61a8b3fae5d0
bruneeee <nikibrunello96@gmail.com>
2014-12-18 19:28:54 +0100

    Mr.Futsal
    Creato il 18/12/14 da Brunello Nicolò

---
+++ app/src/main/java/it/example/nicolobrunello/mrfutsal/squadre.java
@@ -0,0 +1,28 @@
+package it.example.nicolobrunello.mrfutsal;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class squadre extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_squadre, container, false);
+    }
+}
